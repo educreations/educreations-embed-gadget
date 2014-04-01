7be7e4cdb253bee7de59ecfeee127e0679848e85
Shaohua Zhou <shao@versal.com>
2014-03-31 21:14:58 -0700

    Cleanup

--- assets/scripts/app_view.js
+++ assets/scripts/app_view.js
@@ -10,46 +10,34 @@ Educreations.Views.AppView = Backbone.View.extend({
     //house keeping
     _.bindAll(this, 'render', 'renderEmbedCode');
 
-    //author or leaner
+    //author or learner, default to learner
     this.isEditable = false;
 
     //two event buses
     this.ventFromServer = _.extend({}, Backbone.Events);
     this.ventToServer = _.extend({}, Backbone.Events);
 
-    //events with servers
+    //similar to dom ready, fired with the gadget is attached to dom
     this.listenTo(this.ventFromServer, 'attached', function(){
       // console.log('attached');
       this.setPropertySheet();
-      this.renderEmbedCode();
     });
 
+    //the main channel for server to send data back
     this.listenTo(this.ventFromServer, 'attributesChanged', function(messageData){
       // console.log('attributesChanged', messageData);
-      if(messageData && messageData.embedCode){
+      if(messageData && messageData.embedCode){ //guard against undefined
         this.model.set('embedCode', messageData.embedCode);
-      } else {
-        //manually set up default for the fist time
-        //this.model is a Backbone model in memory, meaning not persisted
-        //it can be replaced by a simple object if you don't need change events
-        //local temp changes
-        if(typeof this.model.get('embedCode') === undefined ||
-          $.trim(this.model.get('embedCode')) === '') {
-          this.model.set('embedCode', this.getDefaultEmbedCode());
-        }
-        //persist changes to server
-        this.ventToServer.trigger('setAttributes', {
-          embedCode: this.model.get('embedCode')
-        });
       }
     });
 
+    //server tells whether it is authoring or learning mode
     this.listenTo(this.ventFromServer, 'setEditable', function(editable){
       // console.log('setEditable', editable);
       this.isEditable = editable;
     });
 
-    //lazy render with local backbone model
+    //lazy render based on changes of the local backbone model
     var lazyRenderEmbedCode = _.debounce(this.renderEmbedCode, 300);
     this.listenTo(this.model, 'change', function(){
       // console.log('change');
@@ -58,6 +46,9 @@ Educreations.Views.AppView = Backbone.View.extend({
 
   },
 
+  /**
+   * set up a textarea for authors to put in embed code
+   */
   setPropertySheet: function(){
     this.ventToServer.trigger('setPropertySheetAttributes', {
       embedCode: {
@@ -78,6 +69,9 @@ Educreations.Views.AppView = Backbone.View.extend({
     return input;
   },
 
+  /**
+   * render either the iframe or the error message
+   */
   renderEmbedCode: function(){
     var sanitizedHtml = this.sanitizeStub( this.model.get('embedCode') );
 
@@ -91,22 +85,21 @@ Educreations.Views.AppView = Backbone.View.extend({
       //no iframe, error out
       //note invoking the compiled error template here
       this.$el.find('.gadget-embed').html( JST["assets/templates/error.hbs"]() );
-    }
 
+      //hide the gadet in learner view if it is in error state
+      if(!this.isEditable){
+        this.$el.hide();
+      }
+    }
   },
 
+  /**
+   * intial render to set up basic DOM
+   */
   render: function(){
     this.$el.html( this.template() );
     this.delegateEvents();
     return this;
-  },
-
-  getDefaultEmbedCode: function(){
-    //set up default embed code
-    var defaultEmbedCode = '<iframe width="480" height="300" src="'+
-      'https://www.educreations.com/lesson/embed/444280/" '+
-      'frameborder="0" webkitAllowFullScreen mozallowfullscreen '+
-      'allowfullscreen></iframe>';
-    return defaultEmbedCode;
   }
+
 });
