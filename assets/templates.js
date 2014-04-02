3b692f5ce62ea28f16000c0cb5f5ed0a02470322
Shaohua Zhou <shao@versal.com>
2014-04-01 18:06:09 -0700

    Check in templates.js so that the code would run for the first time without grunt

---
+++ assets/templates.js
@@ -0,0 +1,19 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["assets/templates/app.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+
+
+  return "<div class=\"gadget-embed\"></div>\n";
+  });
+
+this["JST"]["assets/templates/error.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+
+
+  return "<div class=\"gadget-embed-not-valid\">\n  <div class=\"gadget-embed-not-valid-undefined-url\">\n    Please specify the embed code\n  </div>\n</div>\n";
+  });
