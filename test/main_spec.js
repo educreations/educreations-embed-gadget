427054994cfe313f788d6d2487712a33cd2496e4
Jan Paul <janpaul123@users.noreply.github.com>
2014-08-11 14:58:51 -0700

    Included better tests that test behaviour

--- test/main_spec.js
+++ test/main_spec.js
@@ -1,50 +1,23 @@
 /* global describe, it, expect, educreationsGadget */
 
 (function() {
-  describe('Test educreationsGadget', function() {
+  describe('educreationsGadget', function() {
+    it('shows "Please specify the embed code" if no embed code is given', function(done) {
+      window.postMessage({event: 'attributesChanged', data: {}}, '*');
 
-    describe('Gadget level functions', function(){
-      it('should have initialize()', function() {
-        expect(educreationsGadget.initialize).to.be.a('function');
-      });
-      it('should have sendMessage()', function() {
-        expect(educreationsGadget.sendMessage).to.be.a('function');
-      });
+      setTimeout(function() {
+        chai.expect(document.body.textContent).to.contain("Please specify the embed code");
+        done();
+      }, 100);
     });
 
-    describe('AppView level functions', function(){
-      it('should have render()', function() {
-        expect(educreationsGadget.appView.render).to.be.a('function');
-      });
+    it('embeds educreations embed codes', function(done) {
+      window.postMessage({event: 'attributesChanged', data: {embedCode: "<iframe width=\"480\" height=\"300\" src=\"https://www.educreations.com/lesson/embed/444280/\" frameborder=\"0\" webkitAllowFullScreen mozallowfullscreen allowfullscreen></iframe>"}}, '*');
 
-      it('should have renderEmbedCode()', function() {
-        expect(educreationsGadget.appView.renderEmbedCode).to.be.a('function');
-      });
+      setTimeout(function() {
+        chai.expect(document.body.innerHTML).to.contain("<iframe");
+        done();
+      }, 100);
     });
-
-    /**
-     * todo
-     * make it more sophisticated
-     */
-    describe('Fake events', function(){
-      it('should receive attached events', function(done){
-        var errTimeout = setTimeout(function () {
-          expect(false, 'Event never fired').to.equal(true);
-          done();
-          //the following line will work better in the cutting edge version of mocha
-          //needs to wait for mocha to update itself
-          //mocha.throwError('Event never fired');
-        }, 1000); //timeout with an error in one second
-
-        educreationsGadget.appView.ventFromServer.on('attached', function(){
-          clearTimeout(errTimeout); //cancel error timeout
-          expect(true, 'Event fired').to.equal(true);
-          done();
-        });
-
-        educreationsGadget.appView.ventFromServer.trigger('attached');
-      });
-    });
-
   });
 })();
