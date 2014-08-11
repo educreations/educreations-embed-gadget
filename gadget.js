de6d51b9e52c541ee5285804755c4d4ac7211d41
Jan Paul <janpaul123@users.noreply.github.com>
2014-08-11 14:06:55 -0700

    Add all JS to gadget.js to ease refactoring

--- gadget.js
+++ gadget.js
@@ -1,4 +1,16 @@
-/*global Educreations, $, _, Backbone */
+/*global $, _, Backbone */
+
+window.Educreations = {
+  Views: {},
+  init: function (that, thatEl) {
+    that.appModel = new Backbone.Model();
+    that.appView = new Educreations.Views.AppView({
+      model: that.appModel,
+      el: thatEl  //todo, no hardcode
+    });
+    that.appView.render();
+  }
+};
 
 Educreations.Views.AppView = Backbone.View.extend({
   template: function() {
@@ -105,3 +117,73 @@ Educreations.Views.AppView = Backbone.View.extend({
   }
 
 });
+
+(function (Backbone, $, _) {
+
+  /**
+   * main file for Versal gadget
+   * @param {el: DOM node} options
+   */
+  var Gadget = function(options) {
+    this.el = options.el;
+
+    this.initialize();
+
+    //todo
+    //getter and setter for both config and learnerState
+  };
+
+  Gadget.prototype.initialize = function () {
+    // console.log('initialize');
+
+    //the line belows starts a backbone app
+    window.Educreations.init(this, this.el);
+
+    //relay msg from player to gadget
+    window.addEventListener('message', function(msg){
+      if(!msg || !msg.data){
+        return; //server provides no information
+      }
+
+      var msgData = msg.data;
+
+      //return for string response
+      if(typeof msgData === 'string') {
+        console.log('Error. Server sends string instead of object.', msgData);
+        return;
+      }
+
+      //relay events to appView
+      if(msgData.event){
+        this.appView.ventFromServer.trigger(msgData.event, msgData.data);
+      } else {
+        console.log('Server send an response with no event name');
+      }
+    }.bind(this));
+
+    //relay msg from gadget to player
+    this.appView.ventToServer.on('all', function(messageName, messageData){
+      this.sendMessage({
+        event: messageName,
+        data: messageData
+      });
+    }, this);
+
+    this.appView.ventToServer.trigger('startListening');
+  };
+
+  Gadget.prototype.sendMessage = function (msg) {
+    window.parent.postMessage(msg, '*');
+    // console.log([
+    //   'toPlayer', msg.event, msg.data
+    // ]);
+  };
+
+  //starting point of the gadget
+  //expose a global variable for testing purpose
+  //probablly should have used AMD
+  window.educreationsGadget = new Gadget({
+    el: document.querySelector('.main-app')
+  });
+
+})(Backbone, $, _);
